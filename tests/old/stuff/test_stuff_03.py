c054ac073f48258687a5a0beeff4874e0841a0f6
Raphael Pierzina <raphael@hackebrot.de>
2019-03-15 10:21:59 +0100

    Skip test with Python 2 syntax

--- tests/old/stuff/test_stuff_03.py
+++ tests/old/stuff/test_stuff_03.py
@@ -1,9 +1,14 @@
+import pytest
+
 def test_numbers():
     assert 1234 == 1234
 
 
+@pytest.mark.skip(reason="Outdated Python syntax")
 def test_hello_world():
-    print "hello world"
+    # TODO: Can we remove this test?
+    # print "hello world"
+    pass
 
 
 def test_foobar():
