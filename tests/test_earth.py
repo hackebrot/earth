8c4864ffd34c810e0a424b8009d4f030269d8589
Raphael Pierzina <raphael@hackebrot.de>
2019-03-15 12:29:27 +0100

    Write a happy path test

---
+++ tests/test_earth.py
@@ -0,0 +1,32 @@
+import pytest
+
+from earth import adventurers, Event, Months
+
+
+@pytest.mark.wip
+@pytest.mark.happy
+def test_earth():
+    print("Hello adventurers! 🏕")
+    print("-" * 40)
+
+    friends = [
+        adventurers.new_frog("Bruno"),
+        adventurers.new_lion("Michael"),
+        adventurers.new_koala("Brianna"),
+        adventurers.new_tiger("Julia"),
+    ]
+
+    event = Event("PyCon US", "North America", Months.MAY)
+
+    for adventurer in friends:
+        event.invite(adventurer)
+
+    print("-" * 40)
+
+    for attendee in event.attendees:
+        attendee.get_ready()
+        attendee.travel_to(event)
+
+    print("-" * 40)
+
+    event.start()
