39a4a1337c3f2309ce3b03d4dd667871feceb748
Raphael Pierzina <raphael@hackebrot.de>
2019-07-06 10:06:36 +0200

    Implement hook to deselect slow tests by default

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,22 @@ def pytest_addoption(parser):
         default=False,
         help="Include slow tests in test run",
     )
+
+
+def pytest_collection_modifyitems(items, config):
+    """Deselect tests marked as slow if --slow is set."""
+
+    if config.option.slow is True:
+        return
+
+    selected_items = []
+    deselected_items = []
+
+    for item in items:
+        if item.get_closest_marker("slow"):
+            deselected_items.append(item)
+        else:
+            selected_items.append(item)
+
+    config.hook.pytest_deselected(items=deselected_items)
+    items[:] = selected_items
