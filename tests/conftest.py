33ea25c89aa19bcb70a80c8ee7507c23812e5553
Raphael Pierzina <raphael@hackebrot.de>
2019-07-06 17:02:51 +0200

    Write plugin that caches test durations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+from collections import defaultdict
+
+import pytest
+
+
 def pytest_addoption(parser):
     group = parser.getgroup("earth")
     group.addoption(
@@ -25,3 +30,25 @@ def pytest_collection_modifyitems(items, config):
 
     config.hook.pytest_deselected(items=deselected_items)
     items[:] = selected_items
+
+
+class Turtle:
+    """Plugin for adding markers to slow running tests."""
+
+    def __init__(self, config):
+        self.config = config
+        self.durations = defaultdict(dict)
+        self.durations.update(self.config.cache.get("cache/turtle", defaultdict(dict)))
+        self.slow = 5.0
+
+    def pytest_runtest_logreport(self, report):
+        self.durations[report.nodeid][report.when] = report.duration
+
+    def pytest_sessionfinish(self, session):
+        cached_durations = self.config.cache.get("cache/turtle", defaultdict(dict))
+        cached_durations.update(self.durations)
+        self.config.cache.set("cache/turtle", cached_durations)
+
+
+def pytest_configure(config):
+    config.pluginmanager.register(Turtle(config), "turtle")
